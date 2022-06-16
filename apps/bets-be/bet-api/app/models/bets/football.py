badc48e6d5fe300a3a816a7497551a514f307cd0
kozaczinioo <kozaczinioo@wp.pl>
2022-06-16 09:38:02 +0200

    fix: fix mistakes

--- apps/bets-be/bet-api/app/models/bets/football.py
+++ apps/bets-be/bet-api/app/models/bets/football.py
@@ -1,70 +1,72 @@
-from enum import Enum
-from typing import Optional, List
 from . import BetModel
 
-class overall_score(BetModel):
+
+class OverallScore(BetModel):
     bet_type = "overall_score"
     home_win: float
     draw: float
     away_win: float
 
 
-class goals_number(BetModel):
+class GoalsNumber(BetModel):
     bet_type = "goals_number"
-    above_05 = float
-    above_1 = float
-    above_15 = float
-    above_2 = float
-    above_25 = float
-    above_3 = float
-    above_35 = float
-    above_4 = float
-    above_45 = float
-    above_5 = float
-    above_55 = float
-    above_6 = float
-    above_65 = float
-    above_05 = float
-    above_1 = float
-    above_15 = float
-    above_2 = float
-    above_25 = float
-    above_3 = float
-    above_35 = float
-    above_4 = float
-    above_45 = float
-    above_5 = float
-    above_55 = float
-    above_6 = float
-    above_65 = float
+    home_above_05: float
+    home_above_1: float
+    home_above_15: float
+    home_above_2: float
+    home_above_25: float
+    home_above_3: float
+    home_above_35: float
+    home_above_4: float
+    home_above_45: float
+    home_above_5: float
+    home_above_55: float
+    home_above_6: float
+    home_above_65: float
 
-class both_sides_score(BetModel):
+    away_above_05: float
+    away_above_1: float
+    away_above_15: float
+    away_above_2: float
+    away_above_25: float
+    away_above_3: float
+    away_above_35: float
+    away_above_4: float
+    away_above_45: float
+    away_above_5: float
+    away_above_55: float
+    away_above_6: float
+    away_above_65: float
+
+
+class BothSidesScore(BetModel):
     bet_type = "both_sides_score"
     yes: float
     no: float
 
-class halftime_winner(BetModel):
+
+class HalftimeWinner(BetModel):
     bet_type = "halftime_winner"
     home_win: float
     draw: float
     away_win: float
-    
-class first_goal(BetModel):
+
+
+class FirstGoal(BetModel):
     bet_type = "first_goal"
     home_team_scores: float
     away_team_scores: float
 
-class handicap1(BetModel):
+
+class Handicap1(BetModel):
     bet_type = "handicap1"
     home_win: float
     draw: float
     away_win: float
 
-class handicap2(BetModel):
+
+class Handicap2(BetModel):
     bet_type = "handicap2"
     home_win: float
     draw: float
     away_win: float
-
-    
-
--- apps/bets-be/bet-api/app/models/event.py
+++ apps/bets-be/bet-api/app/models/event.py
@@ -1,7 +1,9 @@
 from enum import Enum
 from typing import Optional, List
-from app.bets import BetModel
+
 from pydantic import BaseModel
+
+from .bets import BetModel
 
 
 class OrderByModel(str, Enum):
@@ -9,17 +11,16 @@ class OrderByModel(str, Enum):
     date_inc = "date_inc"
     score_desc = "score_desc"
 
+
 class BaseEventModel(BaseModel):
     id: str
     home_team: str
     away_team: str
     discipline: str
-    
+
+
 class DetailedEventModel(BaseEventModel):
     event_date: str
     created_at: str
     updated_at: Optional[str]
     bets: List[BetModel]
-
-
-    # todo
