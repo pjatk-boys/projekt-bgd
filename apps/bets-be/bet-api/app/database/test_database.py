f78d9648479838263652a94dfcb75444b36a03ba
kozaczinioo <kozaczinioo@wp.pl>
2022-06-17 15:39:21 +0200

    chore: database test get_by_id

--- apps/bets-be/bet-api/app/database/test_database.py
+++ apps/bets-be/bet-api/app/database/test_database.py
@@ -10,3 +10,9 @@ class TestMongoDatabase(TestCase):
         database = MongoDatabase(MONGO_CONNECTION_STRING)
         all = database.get_all()
         self.assertTrue(all)
+
+    def test_get_by_custom_id(self):
+        MONGO_CONNECTION_STRING = os.environ["MONGO_CONNECTION_STRING"]
+        database = MongoDatabase(MONGO_CONNECTION_STRING)
+        all = database.get_by_custom_id('1')
+        self.assertTrue(all)
